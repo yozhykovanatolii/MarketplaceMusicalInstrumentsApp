c2a3b38f9bd892a58e9e8def446511c56cb65d4e
Yozhykov Anatolii <yozhykovanatolii@gmail.com>
2025-09-15 10:58:51 +0300

    Modifying class UserAuth to add method for holding authorization

--- lib/data/datasource/remote/user/user_auth.dart
+++ lib/data/datasource/remote/user/user_auth.dart
@@ -1,1 +1,19 @@
-class UserAuth {}
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserAuth {
+  final _firebaseAuth = FirebaseAuth.instance;
+
+  Future<void> signInWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } catch (exception) {
+      throw Exception(exception.toString());
+    }
+  }
+}
