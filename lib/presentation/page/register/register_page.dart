bb8b9772cf3b087b0fabee5ff7a1797d14c9cf49
Yozhykov Anatolii <yozhykovanatolii@gmail.com>
2025-09-16 12:44:47 +0300

    Adding validation fields to register page

--- lib/presentation/page/register/register_page.dart
+++ lib/presentation/page/register/register_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:marketplace_musical_instruments_app/core/widget/common_button.dart';
 import 'package:marketplace_musical_instruments_app/core/widget/common_text_field.dart';
+import 'package:marketplace_musical_instruments_app/presentation/bloc/register/register_bloc.dart';
+import 'package:marketplace_musical_instruments_app/presentation/bloc/register/register_event.dart';
+import 'package:marketplace_musical_instruments_app/presentation/bloc/register/register_state.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
@@ -31,20 +35,47 @@ class RegisterPage extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: 50),
-              const CommonTextField(
-                prefixIcon: Icons.person,
-                hintText: 'Enter your full name',
+              BlocSelector<RegisterBloc, RegisterState, String?>(
+                selector: (state) => state.fullNameError,
+                builder: (context, fullNameError) {
+                  return CommonTextField(
+                    onChanged: (fullName) => context.read<RegisterBloc>().add(
+                      RegisterFullNameChangeEvent(fullName),
+                    ),
+                    prefixIcon: Icons.person,
+                    hintText: 'Enter your full name',
+                    errorText: fullNameError,
+                  );
+                },
               ),
               const SizedBox(height: 15),
-              const CommonTextField(
-                prefixIcon: Icons.email,
-                hintText: 'Enter your email',
+              BlocSelector<RegisterBloc, RegisterState, String?>(
+                selector: (state) => state.emailError,
+                builder: (context, emailError) {
+                  return CommonTextField(
+                    onChanged: (email) => context.read<RegisterBloc>().add(
+                      RegisterEmailChangeEvent(email),
+                    ),
+                    prefixIcon: Icons.email,
+                    hintText: 'Enter your email',
+                    errorText: emailError,
+                  );
+                },
               ),
               const SizedBox(height: 15),
-              const CommonTextField(
-                prefixIcon: Icons.lock,
-                hintText: 'Enter your password',
-                obscureText: true,
+              BlocSelector<RegisterBloc, RegisterState, String?>(
+                selector: (state) => state.passwordError,
+                builder: (context, passwordError) {
+                  return CommonTextField(
+                    onChanged: (password) => context.read<RegisterBloc>().add(
+                      RegisterPasswordChangeEvent(password),
+                    ),
+                    prefixIcon: Icons.lock,
+                    hintText: 'Enter your password',
+                    obscureText: true,
+                    errorText: passwordError,
+                  );
+                },
               ),
               const SizedBox(height: 30),
               CommonButton(
