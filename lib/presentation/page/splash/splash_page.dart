339613800b02b060949223f7dafb0c72b72087be
Yozhykov Anatolii <yozhykovanatolii@gmail.com>
2025-09-19 12:27:17 +0300

    Updating SplashScreen to check if user is authenticated

--- lib/presentation/page/splash/splash_page.dart
+++ lib/presentation/page/splash/splash_page.dart
@@ -1,12 +1,20 @@
 import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:marketplace_musical_instruments_app/presentation/bloc/app/app_bloc.dart';
+import 'package:marketplace_musical_instruments_app/presentation/bloc/app/app_state.dart';
 import 'package:marketplace_musical_instruments_app/presentation/page/login/login_page.dart';
+import 'package:marketplace_musical_instruments_app/presentation/page/main_page.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final state = context.select((AppBloc appBloc) => appBloc.state);
+    final nextScreen = state is UserAuthenticatedState
+        ? const MainPage()
+        : const LoginPage();
     return FlutterSplashScreen.fadeIn(
       backgroundColor: Colors.blue,
       childWidget: const Icon(
@@ -14,7 +22,7 @@ class SplashPage extends StatelessWidget {
         Icons.store,
         color: Colors.white,
       ),
-      nextScreen: const LoginPage(),
+      nextScreen: nextScreen,
     );
   }
 }
