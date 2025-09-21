30f63ee1fd0d3d1d3b8180c8b0b95f0fd0ec9108
Yozhykov Anatolii <yozhykovanatolii@gmail.com>
2025-09-21 15:44:24 +0300

    Adding GoogleMap to ListingMiniGoogleMap

--- lib/core/widget/listing_mini_google_map.dart
+++ lib/core/widget/listing_mini_google_map.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ListingMiniGoogleMap extends StatelessWidget {
   const ListingMiniGoogleMap({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      height: 200,
+      child: Material(
+        elevation: 5,
+        borderRadius: BorderRadiusGeometry.circular(15),
+        clipBehavior: Clip.hardEdge,
+        child: const GoogleMap(
+          zoomControlsEnabled: false,
+          myLocationEnabled: true,
+          myLocationButtonEnabled: true,
+          initialCameraPosition: CameraPosition(
+            target: LatLng(37.42796133580664, -122.085749655962),
+            zoom: 14,
+          ),
+        ),
+      ),
+    );
   }
 }
