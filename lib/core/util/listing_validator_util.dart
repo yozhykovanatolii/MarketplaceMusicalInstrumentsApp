c0fc61d486cc8a2f1ae39ea22a163f99ff43c677
Yozhykov Anatolii <yozhykovanatolii@gmail.com>
2025-09-23 10:04:59 +0300

    Creating ListingValidatorUtil to manage validation while user is creating or editing a listing

---
+++ lib/core/util/listing_validator_util.dart
@@ -0,0 +1,26 @@
+class ListingValidatorUtil {
+  static String? validateTitle(String title) {
+    if (title.isEmpty) return "The title must not be empty.";
+    if (title.length > 50) return "The title must not exceed 50 characters";
+    return null;
+  }
+
+  static String? validateDescription(String description) {
+    if (description.isEmpty) return "The description must not be empty.";
+    if (description.length > 70) {
+      return "The description must not exceed 70 characters";
+    }
+    return null;
+  }
+
+  static String? validateListingPrice(String priceText) {
+    if (priceText.isEmpty || priceText.startsWith('-')) {
+      return "The price must not be empty or starts with -";
+    }
+    int price = int.parse(priceText);
+    if (price <= 0) {
+      return "The price must not be less than or equal to 0.";
+    }
+    return null;
+  }
+}
