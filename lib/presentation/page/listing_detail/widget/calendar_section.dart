80e23d97860a894ccb90270969d85284fe8cd16b
Yozhykov Anatolii <yozhykovanatolii@gmail.com>
2025-10-04 17:32:55 +0300

    Updating CaledarSection

--- lib/presentation/page/listing_detail/widget/calendar_section.dart
+++ lib/presentation/page/listing_detail/widget/calendar_section.dart
@@ -1,14 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class CalendarSection extends StatelessWidget {
+class CalendarSection extends StatefulWidget {
   const CalendarSection({super.key});
 
   @override
+  State<CalendarSection> createState() => _CalendarSectionState();
+}
+
+class _CalendarSectionState extends State<CalendarSection> {
+  DateTime? _startDate;
+  DateTime? _endDate;
+  DateTime _focusedDay = DateTime.now();
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Placeholder(),
+    final textFontSize = MediaQuery.textScalerOf(context).scale(17);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TableCalendar(
+        daysOfWeekHeight: 20,
+        firstDay: DateTime.now(),
+        lastDay: DateTime.utc(2030, 3, 14),
+        focusedDay: _focusedDay,
+        rangeSelectionMode: RangeSelectionMode.toggledOn,
+        rangeStartDay: _startDate,
+        rangeEndDay: _endDate,
+        onRangeSelected: (start, end, focusedDay) {
+          setState(() {
+            _startDate = start;
+            _endDate = end;
+            _focusedDay = focusedDay;
+          });
+        },
+        headerStyle: HeaderStyle(
+          titleCentered: true,
+          formatButtonVisible: false,
+          titleTextStyle: TextStyle(
+            fontSize: MediaQuery.textScalerOf(context).scale(19),
+            fontWeight: FontWeight.bold,
+          ),
+          leftChevronIcon: const Icon(
+            Iconsax.arrow_square_left_copy,
+            size: 30,
+            color: Colors.blue,
+          ),
+          rightChevronIcon: const Icon(
+            Iconsax.arrow_right_copy,
+            size: 30,
+            color: Colors.blue,
+          ),
+        ),
+        daysOfWeekStyle: DaysOfWeekStyle(
+          weekendStyle: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: textFontSize,
+          ),
+          weekdayStyle: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: textFontSize,
+          ),
+        ),
+        calendarStyle: CalendarStyle(
+          todayTextStyle: TextStyle(
+            fontSize: textFontSize,
+            color: Colors.white,
+          ),
+          rangeStartTextStyle: TextStyle(
+            fontSize: textFontSize,
+            color: Colors.white,
+          ),
+          withinRangeTextStyle: TextStyle(fontSize: textFontSize),
+          weekendTextStyle: TextStyle(fontSize: textFontSize),
+          defaultTextStyle: TextStyle(fontSize: textFontSize),
+          outsideTextStyle: TextStyle(
+            fontSize: textFontSize,
+            color: const Color(0xFFAEAEAE),
+          ),
+          disabledTextStyle: TextStyle(
+            fontSize: textFontSize,
+            color: const Color(0xFFAEAEAE),
+          ),
+        ),
       ),
     );
   }
