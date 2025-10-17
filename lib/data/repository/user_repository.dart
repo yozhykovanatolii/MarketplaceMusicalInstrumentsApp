d03a9371fda9010a2e152efc779e8e8a2daf0bf1
Yozhykov Anatolii <yozhykovanatolii@gmail.com>
2025-10-17 11:21:59 +0300

    Creating UserStorage and updating UserRepository

--- lib/data/repository/user_repository.dart
+++ lib/data/repository/user_repository.dart
@@ -1,14 +1,24 @@
 import 'package:marketplace_musical_instruments_app/core/exception/user_not_found_exception.dart';
+import 'package:marketplace_musical_instruments_app/data/datasource/local/camera/camera_picker.dart';
 import 'package:marketplace_musical_instruments_app/data/datasource/remote/user/user_auth.dart';
 import 'package:marketplace_musical_instruments_app/data/datasource/remote/user/user_firestore.dart';
+import 'package:marketplace_musical_instruments_app/data/datasource/remote/user/user_storage.dart';
 import 'package:marketplace_musical_instruments_app/data/model/user_model.dart';
 
 class UserRepository {
+  final _cameraPicker = CameraPicker();
   final _userAuth = UserAuth();
+  final _userStorage = UserStorage();
   final _userFirestore = UserFirestore();
 
   Future<bool> checkIfUserExistByEmail(String email) async {
     return _userFirestore.checkUserByEmail(email);
+  }
+
+  Future<String> getUserImage() async {
+    final userImageFile = await _cameraPicker.pickImageFileFromGallery();
+    final userImageUrl = await _userStorage.saveUserImage(userImageFile);
+    return userImageUrl;
   }
 
   Stream<UserModel> getUserModelCurrentData() {
